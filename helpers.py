b11734aba2836711555b600a2050cb9f36893f79
CarlaSa <carlachristin@googlemail.com>
2025-12-04 15:50:45 +0100

    added a lot of helper functions from last year

--- helpers.py
+++ helpers.py
@@ -1,6 +1,8 @@
+import random
 import time
 from time import perf_counter_ns
 from pprint import PrettyPrinter
+import numpy as np
 
 def get_input_lines(i):
 
@@ -16,7 +18,12 @@ def get_input(i):
         text = f.read()
     return text.strip()
 
-
+def get_map_input(i):
+    file_name = "inputs/" + f"{i:02}" + ".txt"
+    with open(file_name) as f:
+        text = f.read()
+    # split by lines and then split string into individual characters
+    return [list(t) for t in text.strip().split("\n")]
 
 
 def time_wrapper(func):
@@ -38,3 +45,98 @@ def time_wrapper(func):
             f.write("\n")
         return result_string
     return wrapper
+
+
+color_codes = {
+    "red": '\033[91m',
+    "green": '\033[92m',
+    "blue": '\033[94m',
+    "yellow": '\033[93m',
+    "cyan": '\033[96m',
+    "magenta": '\033[95m',
+    "purple": '\033[96m',
+}
+
+def rgb_color_code(r,g,b):
+    return f"\033[38;2;{r};{g};{b}m"
+
+def simcmap(string, color):
+    if not isinstance(string, str):
+        if isinstance(string, bool) or isinstance(string, np.bool_):
+            string = f"{str(string):6}"
+        elif isinstance(string, int) or isinstance(string, float):
+            string = f"{string:3}"
+        else:
+            try:
+                string = f"{string:3}"
+            except:
+                print(type(string))
+                raise NotImplementedError
+
+    if color is not None:
+        if color in color_codes:
+            CSTART = color_codes[color]
+        elif len(color) == 3: # rgb
+            CSTART = rgb_color_code(*color)
+        CEND = '\033[0m'
+        repr = CSTART + string + CEND
+    else:
+        repr = string
+
+    return repr
+
+
+def np_repr(arr : np.ndarray) -> str:
+    representation = ""
+    assert len(arr.shape) == 2
+    for line in arr:
+        representation += " ".join(str(elem) for elem in line)
+        representation += "\n"
+    return representation
+
+
+# def test_grid():
+#     string = ""
+#     step = 40
+#     for g in range(0, 256, 16):
+#         for b in range(0, 256, 16):
+#             for r in range(0, 256, 16):
+#                 string += simcmap("test", (r,g,b))
+#                 string += " "
+#             string += "\n"
+#     print(string)
+
+# def get_n_colors(n):
+#     # doesn't have to be good lol
+#     samples = list()
+#     for g in range(0, 256, 16):
+#         for b in range(0, 256, 16):
+#             for r in range(0, 256, 16):
+#                 samples.append((r,g,b))
+#
+#     random.shuffle(samples)
+#     return samples[:n]
+
+
+def np_repr_unique(arr : np.ndarray) -> str:
+    unique_values = np.unique(arr)
+
+    arr_copy = arr.copy()
+    if arr_copy.dtype != "object":
+        arr_copy = arr_copy.astype("object")
+    if len(unique_values) < len(color_codes):
+        colors = iter(color_codes.keys())
+    else:
+        pass
+        # colors = iter(get_n_colors(len(unique_values)))
+    for val in unique_values:
+        next_color = next(colors, None)
+        colored_value = simcmap(val, next_color)
+        arr_copy[arr_copy == val] = colored_value
+    return np_repr(arr_copy)
+
+def np_color_print(arr : np.ndarray):
+    print(np_repr_unique(arr))
+
+def np_print(arr : np.ndarray):
+    print(np_repr(arr))
